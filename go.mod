ded5484d40ee6221e4b640f3fa793c2e326b14ac
herrhu <herrhu@herrhudeMacBook-Air.local>
2022-08-31 01:44:05 +0800

    feat:simple lexer implement

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/herrhu97/monkey
+
+go 1.16
---
+++ token/token.go
@@ -0,0 +1,65 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 标识符 + 字面量
+	IDENT = "IDENT" // add, foobar, x, y ...
+	INT   = "INT"   // 123
+
+	// 运算符
+	ASSIGN   = "="
+	PLUS     = "+"
+	BANG     = "!"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	// 分隔符
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 关键字
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type TokenType string
+
+// Token 词法单元
+type Token struct {
+	Type    TokenType // 词法单元类型
+	Literal string    // 字面量
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
