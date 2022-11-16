87ca49b0577b0ced5ff8f8e697f7261b6ff6a580
herrhu97 <herrhu@qq.com>
2022-11-16 20:55:55 +0800

    feat: 1

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,6 +31,10 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 
 	switch l.ch {
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
 	case '"':
 		tok.Type = token.STRING
 		tok.Literal = l.readString()
